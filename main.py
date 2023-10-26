46a7929c6219504d10b33cdf7755fa3493a740a1
Edward Bruin <edwardnbruin@gmail.com>
2023-10-26 22:05:46 +1100

    Roadshow ready notebooks

--- main.py
+++ main.py
@@ -12,7 +12,189 @@ from torch.utils.data import Dataset
 import random
 import re
 from datetime import datetime
+import html
+import numpy as np
 
+def benchmark(model, tokenizer, n=100, checkvalid=False, verbose=False):
+    from datetime import datetime as dt
+    times = []
+    for i in range(n):
+        if i% (n//10) == 0:
+            print(f'{i*10//n}% complete')
+        now = dt.now()
+        result = demo(model, tokenizer)
+        delta = dt.now()-now
+        if checkvalid and result['short'] not in result['predicted']:
+            delta = now-dt.now()
+        times.append(delta.total_seconds())
+        if verbose:
+            print(delta)
+    return times
+
+def quantized_load(model_id="vilsonrodrigues/falcon-7b-instruct-sharded"):
+    import torch
+    from transformers import BitsAndBytesConfig, AutoModelForCausalLM, AutoTokenizer, pipeline
+    import json
+    
+    quantization_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_compute_dtype=torch.float16,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_use_double_quant=True
+    )
+    
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id,
+        quantization_config=quantization_config
+        , trust_remote_code=True
+    )
+    print("model loaded")
+    return(model, tokenizer)
+
+def make_pipeline(model, tokenizer):
+    from transformers import pipeline
+    pipeline_new = pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        use_cache=True,
+        device_map="auto",
+        max_length=150,
+        do_sample=True,
+        top_k=10,
+        num_return_sequences=1,
+        eos_token_id=tokenizer.eos_token_id,
+        pad_token_id=tokenizer.eos_token_id, 
+        return_full_text=False
+    )
+    return(pipeline_new)
+
+def process_sequence(text):
+    processed = ''
+    import re
+
+    pattern = r'\[/?(LONG|SHRT|MESS)\][^\[]*'
+    result = re.sub(pattern, '', text, flags=re.DOTALL)
+    return result
+
+def demo(model, tokenizer, choose=False):
+    import random
+    input_file = "./sequentialised.json"
+    with open(input_file, 'r') as f:
+        data = json.load(f)
+
+    index = random.randrange(len(data))
+    selected_data = data[index]
+    
+    if choose == True:
+        print(f"data length: {len(data)}")
+        user_input = input(">> select index:")
+    
+        if user_input.isdigit():
+            index = int(user_input)
+            if index > 0 and index < len(data):
+                selected_data = data[index - 1]
+    
+    seed, actual = selected_data['text'].split('[LONG]')
+    pipeline = make_pipeline(model, tokenizer)
+    sequences = pipeline(seed)
+    text = sequences[0]['generated_text']
+    predicted = process_sequence(text)
+    #print(f'seed: {seed}')
+    #print(f'predicted: {predicted}')
+    #print(f'actual: {actual}')
+
+    pattern = r"\[SHRT\](.*?)\[/SHRT\]"
+    short = re.findall(pattern, selected_data['text'])[0]
+    
+    result = {
+        'original': selected_data['text'],
+        'seed': seed,
+        'short':short,
+        'actual':actual,
+        'predicted': predicted,
+        'index':index
+    }
+    return(result)
+
+def link_messages(file_path):
+    # Define a function to find message pairs with alternating authors.
+    def find_author_pairs(data):
+        unique_users = set()
+        
+        for d in data:
+            unique_users.add(d["user"])
+        #print("Unique users:", unique_users)
+        unique_users_list = list(unique_users)
+    
+        # Print the list with index numbers
+        print("Unique users:")
+        for index, user in enumerate(unique_users_list):
+            print(f"{index + 1}: {user}")
+        
+        user_input = input(">> select index:")
+        if not user_input.isdigit():
+            print('not digit')
+            return(False)
+    
+        index = int(user_input)
+        if index == 0:
+            print('no index')
+            return(False)
+        
+        if index > len(unique_users_list):
+            print('no index')
+            return(False)
+        
+        try:
+            selected_user = unique_users_list[index - 1]
+        except:
+            print('no index')
+            return(False)
+        print(selected_user)
+        
+        author_a = selected_user
+        
+        # Initialize an empty list to store the message pairs.
+        message_pairs = []
+        # Initialize variables to keep track of the current prompt's author and previous author.
+        current_prompt = None
+        prev_author = None
+    
+        # Get the author of the first message in the data.
+        #author_a = data[0]['user']
+        # Get the content of the first message in the data.
+        prev_message_content = data[0]['message']
+    
+        # Iterate through the list of messages in the data.
+        for message in data:
+            # Get the author and message content of the current message.
+            current_author = message['user']
+            message_content = message['message']
+    
+            # Check if the previous author is the same as author_a, and the current author is different.
+            if prev_author == author_a and current_author != author_a:
+                # If the conditions are met, append the previous message as a "message" and the current message as a "response" to the message_pairs list.
+                message_pairs.append({"message": prev_message_content,'prompt':'', "response": message_content})
+    
+            # Update the previous author and previous message content for the next iteration.
+            prev_author = current_author
+            prev_message_content = message_content
+            message_a = message
+    
+        # Return the list of message pairs.
+        return message_pairs
+    
+    data = []
+    input_file = file_path
+    with open(input_file, 'r') as f:
+        data = json.load(f)
+    
+    # Call the find_author_pairs function on the data to find pairs of messages from alternating authors.
+    message_pairs = find_author_pairs(data)
+    return(message_pairs)
 
 def valid_row(row):
   # word blacklist file is located at ...
@@ -48,10 +230,11 @@ def convert_to_json(file_path):
             messages = re.finditer('<span class=chatlog__markdown-preserve>[^<]+', line)
             for iter in messages:
                 iter_message = iter.group().split(">")[1]
+                decoded_string = html.unescape(iter_message)
                 out_data.append({
                     "user":user,
                     "time":stripped_time,
-                    "message":iter_message
+                    "message":decoded_string
                 })
         else:
             #print(f'no data found in row:\n {line}')
@@ -91,9 +274,23 @@ def calculate_prompt(message, response, limit = 1):
     #results = sorted(prompt_words, key=calculate_word_uniqueness, reverse=True)
     return(results[:limit])
 
+def sequentialise(input_file_path):
+    sequences = []
+    
+    with open(input_file_path, "r") as input_file:
+        input_data = json.load(input_file)
+    
+    for section in input_data:
+
+        message = section["message"]
+        response = section["response"]
+        prompt = " ".join(section["prompt"])
+    
+        sequences.append({'text': f'[MESG]{message}[/MESG][SHRT]{prompt}[/SHRT][LONG]{response}[/LONG]'})
+    print("complete")
+    return(sequences)
 
 def calculate_prompts(input_file_path):
-
     # Load a pre-trained spaCy model with word embeddings (e.g., en_core_web_md)
     
     print("loading parameters")
@@ -106,12 +303,24 @@ def calculate_prompts(input_file_path):
     
     with open(input_file_path, "r") as input_file:
         input_data = json.load(input_file)
-
+    
+    n = len(input_data)
+    #n = 20  # Replace 20 with your desired value of n
+    num_values = 11
+    
+    # Generate 10 evenly spaced numbers ranging from 1 to n
+    evenly_spaced_numbers = list(np.linspace(1, n, num_values, dtype=int))
+    evenly_spaced_numbers = evenly_spaced_numbers[:-1]
+    
     working_data = input_data
     
     # Calculate prompt words for each section using word subtraction
     for i, section in enumerate(working_data):
-
+        try:
+            print(f"current progress: {evenly_spaced_numbers.index(i)*10:.0f}%")
+        except:
+            None
+            
         message = section["message"]
         response = section["response"]
     
